d79cdec3e30db9eb2f2b685294bff95b933c682d
Donederscore <edecoteau@marylandinternationalschool.org>
2020-10-27 09:42:12 -0400

    Update homescreen.py

--- HomeScreen/homescreen.py
+++ HomeScreen/homescreen.py
@@ -1,9 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/home')
-def test():
-  return 'A home screen will be here once we are done'
-
-app.run()
+#I will put something here later
