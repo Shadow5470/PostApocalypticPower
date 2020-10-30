20221970a183672885c634f25b99c130ecaac633
Tradernfl676 <nikolosk676@gmail.com>
2020-10-29 22:47:18 -0400

    actually making it work

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+alert("Welcome to our interactive game on energy sources. Due to the zombie apocolypse, part of this game could not be finished. Feel free to still enjoy our minigames and have fun!");
 function start(){
   window.location.href = "window.location.href = "https://shadow5470.github.io/PostApocalypticPower/Midway/";";
 }
